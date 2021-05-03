d66ac4b79fb687800c6b724e733fc97d7bac0f7a
Michał <Micha@Ytcvc>
2021-05-03 23:06:50 +0200

    Próba animacji lasera

--- src/testy/Arrow.java
+++ src/testy/Arrow.java
@@ -1,35 +1,59 @@
 package testy;
 
 import java.awt.*;
-import java.awt.geom.AffineTransform;
-
 
 public class Arrow {
 	
-	int[] x = {74,100,126};
-	int[] y = {195,150,195};
-	private Color color = Color.BLUE;
-	private int []vX = {1, 1, 1};
-	private int []vY = {1, 1, 1};
 	private int width = 2;
 	
 	
 	
+	int[] x = {74, 100, 126};
+	int[] y = {195, 150, 195};
 	
-	public void setXs(int[] x) {
+	int[] x1 = {x[1] - width/2, x[1] + width/2, x[1] + width/2, x[1] - width/2};
+	int[] y1 = {y[0]		  , y[0]		  , y[2] + (y[2] - y[1])     , y[2] + (y[2] - y[1])};
+	
+	private Color color = new Color(154, 227, 192, 100);
+	private int []vX = {1, 1, 1, 1};
+	private int []vY = {1, 1, 1, 1};
+	
+	
+	
+	
+	
+	public void setXts(int[] x) {
 		this.x = x;
 	}
 	    
-	public void setYs(int[] y) {
+	public void setYts(int[] y) {
 		this.y = y;
 	}
 	
-	public int[] getXs() {
+	public int[] getXts() {
 		return x;
 	}
 	    
-	public int[] getYs() {
+	public int[] getYts() {
 		return y;
+	}
+	
+	
+	
+	public void setXrs(int[] x1) {
+		this.x1 = x1;
+	}
+	    
+	public void setYrs(int[] y1) {
+		this.y1 = y1;
+	}
+	
+	public int[] getXrs() {
+		return x1;
+	}
+	    
+	public int[] getYrs() {
+		return y1;
 	}
 	
 	
@@ -62,14 +86,14 @@ public class Arrow {
 	public void setColor(Color color) {
 		this.color = color;
 	}
-	
-	
+
 	
 	
 
 	public void paint(Graphics g){
 		g.setColor(color);
 		g.fillPolygon(x, y, 3);
-		g.fillRect(x[0] + (x[2] - x[1])-width/2, y[2], width, y[2]-y[1]);
+	//	g.fillRect(x[0] + (x[2] - x[1])-width/2, y[2], width, y[2]-y[1]);
+		g.fillPolygon(x1, y1, 4);
     }
 }
---
+++ src/testy/ArrowsPanel.java
@@ -0,0 +1,144 @@
+package testy;
+
+
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import javax.swing.JPanel;
+
+
+public class ArrowsPanel extends JPanel implements Runnable{
+	private static final long serialVersionUID = 1L;
+	
+	List<Arrow> arrows = new ArrayList<Arrow>();
+	
+
+	private int a = 11;
+	private int h = (int)(a * Math.sqrt(3) / 2) + 1;
+	private int w = 2;
+	
+
+	private int x = -240, y = 40;
+	
+	private int[] yV = {0, 0, 0, 0};
+	private boolean zero = true;
+	
+	double k;
+	
+	
+	public void addArrow(){
+			
+		
+	Random r = new Random();
+		
+		Color col1 = new Color(r.nextInt(255),r.nextInt(255),r.nextInt(255));
+		
+		
+		
+
+		int[] xt = {x , x + (int)(a/2), x + a};
+		int[] yt = {y + h, y, y + h};
+		
+		int[] xr = {xt[1] - w/2, xt[1] + w/2 + 1, xt[1] + w/2 + 1                 , xt[1] - w/2};
+		int[] yr = {yt[0]      , yt[0]	    , yt[2] + (yt[2] - yt[1])     , yt[2] + (yt[2] - yt[1])};
+		
+		
+		Arrow p = new Arrow();
+		p.setXts(xt);
+		p.setYts(yt);
+		p.setXrs(xr);
+		p.setYrs(yr);
+
+		
+
+		
+		p.setvY(yV);
+		p.setColor(col1);
+		
+		arrows.add(p);	
+		
+		x += 80;
+		
+		
+		if(xt[2] >= 840) {
+			if(zero == true) {
+				x = 40;
+				zero = false;
+			}
+			else {
+				x = 0;
+				zero = true;
+			}
+			y = y + 60;
+			
+		}
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		Graphics2D g2d = (Graphics2D) g;
+		
+		g2d.rotate(Math.toRadians(k), 850/2, 500/2);
+		
+		for (Arrow pr : arrows)
+			pr.paint(g2d);
+	}
+	
+	@Override
+	public void run() {
+		for (Arrow ar : arrows) {
+				
+				ar.setYts(sub(ar.getYts(), yV));
+				ar.setYrs(sub(ar.getYrs(), yV));
+
+				
+				if((ar.getYts()[1] + h + ar.getHeight()) <= -240) {
+					ar.setYts(addnum(ar.getYts()));
+					ar.setYrs(addnum(ar.getYrs()));
+				}
+		}
+		repaint();
+	}
+	
+	int[] add(int []x1, int []x2){
+		int[] ret = new int[x1.length];
+			
+		for(int i = 0; i < x1.length; i++)
+			ret[i] = x1[i] + x2[i];
+			
+		return ret;
+	}
+		
+	int[] addnum(int[] y1){
+		int[] ret = new int[y1.length];
+			
+		for(int i = 0; i < y1.length; i++)
+			ret[i] = y1[i] + 1200;
+			
+		return ret;
+	}
+		
+	int[] sub(int[] x1, int[] x2){
+		int[] ret = new int[x1.length];
+			
+		for(int i = 0; i<x1.length; i++)
+			ret[i] = x1[i] - x2[i];
+			
+		return ret;
+	}
+	
+	void setyV(int a) {
+		int[] yv = {a, a, a, a};
+		yV = yv;
+	}
+	
+	void setk(double k) {
+		this.k = k;
+	}
+}
